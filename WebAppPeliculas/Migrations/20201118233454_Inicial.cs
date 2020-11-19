25adb01a2c2eb2d5a181579c63253413cf58d689
JoaquinMartina <joaquinmartina44@gmail.com>
2020-11-18 21:06:18 -0300

    TP finalizado

---
+++ WebAppPeliculas/Migrations/20201118233454_Inicial.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppPeliculas.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Generos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Descripcion = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Generos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(nullable: false),
+                    Apellido = table.Column<string>(nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(nullable: false),
+                    Biografia = table.Column<string>(nullable: false),
+                    FotoCarnet = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Peliculas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Titulo = table.Column<string>(nullable: false),
+                    FechaEstreno = table.Column<DateTime>(nullable: false),
+                    FotoCartel = table.Column<string>(nullable: true),
+                    Trailer = table.Column<string>(nullable: false),
+                    Resumen = table.Column<string>(nullable: false),
+                    GeneroId = table.Column<int>(nullable: false),
+                    Cartelera = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Peliculas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Peliculas_Generos_GeneroId",
+                        column: x => x.GeneroId,
+                        principalTable: "Generos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PeliculasActores",
+                columns: table => new
+                {
+                    PeliculaId = table.Column<int>(nullable: false),
+                    PersonaId = table.Column<int>(nullable: false),
+                    PapelActor = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PeliculasActores", x => new { x.PersonaId, x.PeliculaId });
+                    table.ForeignKey(
+                        name: "FK_PeliculasActores_Peliculas_PeliculaId",
+                        column: x => x.PeliculaId,
+                        principalTable: "Peliculas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PeliculasActores_Personas_PersonaId",
+                        column: x => x.PersonaId,
+                        principalTable: "Personas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PeliculasGeneros",
+                columns: table => new
+                {
+                    PeliculaId = table.Column<int>(nullable: false),
+                    GeneroId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PeliculasGeneros", x => new { x.PeliculaId, x.GeneroId });
+                    table.ForeignKey(
+                        name: "FK_PeliculasGeneros_Generos_GeneroId",
+                        column: x => x.GeneroId,
+                        principalTable: "Generos",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_PeliculasGeneros_Peliculas_PeliculaId",
+                        column: x => x.PeliculaId,
+                        principalTable: "Peliculas",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Peliculas_GeneroId",
+                table: "Peliculas",
+                column: "GeneroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PeliculasActores_PeliculaId",
+                table: "PeliculasActores",
+                column: "PeliculaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PeliculasGeneros_GeneroId",
+                table: "PeliculasGeneros",
+                column: "GeneroId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PeliculasActores");
+
+            migrationBuilder.DropTable(
+                name: "PeliculasGeneros");
+
+            migrationBuilder.DropTable(
+                name: "Personas");
+
+            migrationBuilder.DropTable(
+                name: "Peliculas");
+
+            migrationBuilder.DropTable(
+                name: "Generos");
+        }
+    }
+}
